b64d7baad947786bc30fadc3e0adff2312ec6ff6
Karthikdurai1994 <duraikarthik6@gmail.com>
2020-10-09 12:50:22 +0530

    Added body parser json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 
 app.get("/", (req, res)=>{
     res.status(200);
