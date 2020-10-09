0ad0ff8888cbaa44ddae501c83bd5cefae288572
Karthikdurai1994 <duraikarthik6@gmail.com>
2020-10-09 12:25:26 +0530

    post request inserted

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+var bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,6 +8,10 @@ app.get("/", (req, res)=>{
     res.send("Hello World");
 })
 
+app.post("/resumetoBase64", (req, res)=>{
+    
+    res.send(req.body);
+})
 app.listen(port, ()=>{
     console.log("Server is listening on PORT: "+port);
 })
